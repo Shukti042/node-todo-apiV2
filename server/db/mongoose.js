001b026af9f51a5afa48e1382557df30a1f2efed
Shukti <nazrinshukti@gmail.com>
2020-04-21 21:29:15 +0600

    Printing MongoDB URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+console.log('--------MongoDB URI---------');
+console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }).
 catch(error => handleError(error));
 
